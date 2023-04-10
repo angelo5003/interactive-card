786fce2735a5048b5009a95b72bcd582c2448917
Tony5300 <tony_tolen@ziggo.nl>
2023-04-10 08:56:48 +0200

    feat: finished the styling of the success message component. Inputs will not be cleared when you click on confirm, but in the success screen when you click on continue the input fields will be cleared.

--- src/components/SuccesMessage/SuccessMessage.jsx
+++ src/components/SuccesMessage/SuccessMessage.jsx
@@ -2,14 +2,24 @@ import React from "react";
 import "./SuccessStyles.css";
 import succesLogo from "../assets/images/icon-complete.svg";
 
-const SuccessMessage = () => {
+const SuccessMessage = ({ setForm }) => {
+  const handleClearInput = () => {
+    setForm({
+      cardnumber: "0000 0000 0000 0000",
+      expdate: "",
+      cardyear: "00/00",
+      cvc: "123",
+    });
+  };
   return (
     <section className="succes_container">
       <div className="succes_info_container">
         <img src={succesLogo} alt="succes_logo" className="icon_complete" />
         <h2 className="succes_title">Thank you!</h2>
         <p className="succes_message">We've added your card details</p>
-        <button className="submit_btn">Continue</button>
+        <button className="submit_btn" onClick={handleClearInput}>
+          Continue
+        </button>
       </div>
     </section>
   );
