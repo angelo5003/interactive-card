7482aace894bcd38662e5c9f5bff7d0da5977df7
Tony5300 <tony_tolen@ziggo.nl>
2023-04-10 15:04:49 +0200

    feat: added the form validation, but it is not working properly.

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,10 +5,26 @@ import SuccessMessage from "../SuccesMessage/SuccessMessage";
 const Form = () => {
   const [form, setForm] = useState({
     cardnumber: "0000 0000 0000 0000",
-    expdate: "",
+    expdate: "0",
     cardyear: "00/00",
     cvc: "123",
   });
+  const [usernameError, setUsernameError] = useState("");
+  const [cardNumberError, setCardNumberError] = useState("");
+  const [expDateError, setExpDateError] = useState("");
+  const [cardYearError, setCardYearError] = useState("");
+  const [cvcError, setCvcError] = useState("");
+
+  console.log(
+    `userName error:`,
+    usernameError,
+    ` cardNumberError:`,
+    cardNumberError,
+    `cardYearError:`,
+    cardYearError,
+    `cvcError:`,
+    cvcError
+  );
 
   const [successMessage, setSuccesMessage] = useState(false);
 
@@ -22,16 +38,50 @@ const Form = () => {
 
   const handleChange = (e) => {
     const { name, value } = e.target; // destructure the name and value property from the e.target object.
-
     setUser({ ...user, [name]: value });
-
     setForm({ ...form, [name]: +value }); //  is updating the form state with the new value of the input field. The { ...form } part is creating a copy of the current state of form (object), and the [name]: value part is updating the property with the same name as the input field (like "username" or "cardnumber") to the new value. The + sign for the value is turning a string value to a number
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    // Simple form validation
+    // if (!user.username || user.username.length <= 0) {
+    //   setUsernameError("Please enter your name");
+    //   return;
+    // } else {
+    //   setUsernameError("");
+    // }
+
+    // if (!form.cardnumber || form.cardnumber.length >= 16) {
+    //   setCardNumberError("Please enter a valid card number");
+    //   return;
+    // } else {
+    //   setCardNumberError("");
+    // }
+
+    // if (!form.expdate || form.expdate.length !== 2) {
+    //   setExpDateError("Please enter a valid expiration date");
+    //   return;
+    // } else {
+    //   setExpDateError("");
+    // }
+
+    // if (!form.cardyear || form.cardyear.length !== 5) {
+    //   setCardYearError("Please enter a valid expiration date");
+    //   return;
+    // } else {
+    //   setCardYearError("");
+    // }
+
+    // if (!form.cvc || form.cvc.length !== 3) {
+    //   setCvcError("Please enter a valid CVC number");
+    //   return;
+    // } else {
+    //   setCvcError("");
+    // }
+    // If all the validation checks pass, show the success message
     showSuccesMessage();
-    console.log(`your form has been submitted`);
   };
 
   return (
@@ -57,29 +107,35 @@ const Form = () => {
           <SuccessMessage setForm={setForm} setUser={setUser} />
         ) : (
           <>
-            <label className="title">CARDHOLDER NAME</label>
-            <input
-              type="text"
-              placeholder="e.g. Jane Appleseed"
-              name="username"
-              value={user.username === "Jane Appleseed" ? "" : user.username} // compare if the state is equal to the default value, if it is change it to an empty string and show the new typed value
-              onChange={handleChange}
-              className="form_input"
-              required
-            />
+            <label className="title" id="cardholder_name">
+              CARDHOLDER NAME
+              <input
+                type="text"
+                placeholder="e.g. Jane Appleseed"
+                name="username"
+                value={user.username === "Jane Appleseed" ? "" : user.username} // compare if the state is equal to the default value, if it is change it to an empty string and show the new typed value
+                onChange={handleChange}
+                className="form_input"
+              />
+              {usernameError && <span>{usernameError}</span>}
+            </label>
 
-            <label className="title">CARD NUMBER</label>
-            <input
-              type="text"
-              placeholder="e.g. 1234 5678 9123 0000"
-              name="cardnumber"
-              value={
-                form.cardnumber === "0000 0000 0000 0000" ? "" : form.cardnumber
-              }
-              onChange={handleChange}
-              className="form_input"
-              required
-            />
+            <label className="title" id="card_number">
+              CARD NUMBER
+              <input
+                type="text"
+                placeholder="e.g. 1234 5678 9123 0000"
+                name="cardnumber"
+                value={
+                  form.cardnumber === "0000 0000 0000 0000"
+                    ? ""
+                    : form.cardnumber
+                }
+                onChange={handleChange}
+                className="form_input"
+              />
+              {cardNumberError && <span>{cardNumberError}</span>}
+            </label>
 
             <div className="card_dates">
               <label className="title">
@@ -88,12 +144,12 @@ const Form = () => {
                   type="text"
                   placeholder="MM"
                   name="expdate"
-                  value={form.expdate}
+                  value={form.expdate === "0" ? "" : form.expdate}
                   onChange={handleChange}
                   className="form_input_dates"
                   id="form_input_expiry_date"
-                  required
                 />
+                {expDateError && <span>{expDateError}</span>}
               </label>
 
               <label className="title">
@@ -106,8 +162,8 @@ const Form = () => {
                   onChange={handleChange}
                   className="form_input_dates"
                   id="form_input_year_date"
-                  required
                 />
+                {cardYearError && <span>{cardYearError}</span>}
               </label>
 
               <label className="title">
@@ -119,10 +175,11 @@ const Form = () => {
                   value={form.cvc === "123" ? "" : form.cvc}
                   onChange={handleChange}
                   className="form_input_dates"
-                  required
                 />
+                {cvcError && <span>{cvcError}</span>}
               </label>
             </div>
+
             <button type="submit" className="submit_btn">
               Confirm
             </button>
